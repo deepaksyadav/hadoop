6c6e2f3d2a31385a3b864f8edc01e3a655de407c
deepaksyadav <deepaksyadav@users.noreply.github.com>
2025-10-14 12:04:05 +0530

    Update Spark_Hadoop.py

--- Spark_Hadoop.py
+++ Spark_Hadoop.py
@@ -14,7 +14,7 @@ myschema = StructType([\
 
 people = spark.read.format("csv")\
     .schema(myschema)\
-    .option("path","hdfs://sandbox-hdp.hortonworks.com:8020/user/maria_dev/spark/friends.csv")\
+    .option("path","hdfs://user/maria_dev/spark/friends.csv")\
     .load()
 
 people.printSchema()
@@ -33,5 +33,6 @@ output.write\
 .option("path", "hdfs:///user/maria_dev/spark/job_output/")\
 .partitionBy("age")\
 .save()
+
 
 
