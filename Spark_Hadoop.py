234e018cadfb9878ff6e0f7bc259508060d4c57e
deepaksyadav <deepaksyadav@users.noreply.github.com>
2025-10-14 12:06:01 +0530

    Update Spark_Hadoop.py

--- Spark_Hadoop.py
+++ Spark_Hadoop.py
@@ -30,9 +30,10 @@ spark.sql("select userID, name from peoples where friends > 100 order by userID"
 
 output.write\
 .format("json").mode("overwrite")\
-.option("path", "hdfs:///user/maria_dev/spark/job_output/")\
+.option("path", "hdfs:///home/maria_dev/spark/job_output/")\
 .partitionBy("age")\
 .save()
+
 
 
 
