a46db4dd36f67e396a47d971792cc3fa91c4fe63
deepaksyadav <deepaksyadav@users.noreply.github.com>
2025-10-14 10:32:38 +0530

    Add files via upload

---
+++ create_rdd.py
@@ -0,0 +1,4 @@
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.appName("RDD example").getOrCreate()
+df = spark.sparkContext.parallelize([(1,2,3,'a b c'), (4,5,6, 'd e f'), (7,8,9, 'g h i')]).toDF(['col1', 'col2', 'col3', 'col4'])
+df.show()
